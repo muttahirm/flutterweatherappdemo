fb2c7768b5811c7fc427489c75bc5855824dd9f0
Muttahir Mumtaz <mumtaz.muttahir@gmail.com>
2024-06-22 23:02:54 +0200

    flutter project and screen

---
+++ android/app/src/main/kotlin/com/example/flutterweatherappdemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterweatherappdemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/ui/screens/main_screen/main_weather_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatefulWidget {
+  const WeatherScreen({super.key});
+  @override
+  State<WeatherScreen> createState() => _WeatherScreenState();
+}
+
+class _WeatherScreenState extends State<WeatherScreen> {
+  final TextEditingController textEditingController = TextEditingController();
+  @override
+  void dispose() {
+    textEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 36),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Friday",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
